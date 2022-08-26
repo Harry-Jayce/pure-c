7958ec307a6243c44b693f547aba5221fd088307
Kusto Mancher <cherrise28@outlook.com>
2022-08-26 12:03:31 +0800

    add pointer

---
+++ pointer/example/pointer_def.c
@@ -0,0 +1,13 @@
+// this example just for simple
+// omit the necessary statements
+// just taste it
+int a = 3;
+int* p = &a;
+p++;
+printf("a=%d\n", a);
+printf("p=%p\n", p);
+/* Output:
+hello world
+a=3
+p=000000E1318FFC98
+*/
